f762ee52ae6414972deb70af9ef9f0bc73eda86f
Cormac Dorrat <cormo905@gmail.com>
2024-03-12 12:53:21 +1100

    feat(tutorial): added amazing feature
    Some further information about what was added in this commit.

---
+++ MonstieWash/Assets/Scripts/TutorialFeature.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialFeature : MonoBehaviour
+{
+    //Hello, this is a file for the git tutorial!
+    //Pretend that there's lots of great code in here
+}
