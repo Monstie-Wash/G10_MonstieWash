3b587d180afd5594a6151611c75f430f0c82f7d1
Cormac Dorrat <cormo905@gmail.com>
2024-03-12 13:18:28 +1100

    feat(print secrets): added secret printing
    This will allow people to print out whatever secret number and message they like :D

--- MonstieWash/Assets/Scripts/TutorialFeature.cs
+++ MonstieWash/Assets/Scripts/TutorialFeature.cs
@@ -6,4 +6,12 @@ public class TutorialFeature : MonoBehaviour
 {
     //Hello, this is a file for the git tutorial!
     //Pretend that there's lots of great code in here
+    private float moveSpd = 5f;
+    private int secretNumber = 102946;
+    private string secretMessage = "Hello, World!";
+
+    private void Start()
+    {
+        Debug.Log($"Secret number is: {secretNumber} | Secret message is: {secretNumber}");
+    }
 }
