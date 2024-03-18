f740235b7caf12b44fabbceef7a1f6899db6261d
NATHAN TRAU <103618066@student.swin.edu.au>
2024-03-19 00:31:32 +1100

    feat(hand boundary checks): added boundary checks to prevent player hand from leaving off screen
    Added boundary check on hand's WorldToScreenPoint() against screen width/height before processing movement input
    Renamed speed float to cursorSpeed to be more descriptive

--- MonstieWash/Assets/Scripts/PlayerHand.cs
+++ MonstieWash/Assets/Scripts/PlayerHand.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Playerhand : MonoBehaviour
 {
+    //Unity Inspector
 	[SerializeField]
-	private float speed = 20;
+    [Range(1.0f, 50.0f)]
+	private float cursorSpeed = 20;
+
+    //Private
+    private Transform m_Transform;
+    private Vector3 handPosition;
 	private float moveHorizontal;
 	private float moveVertical;
-	private Transform m_Transform;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +24,29 @@ public class Playerhand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        handPosition = Camera.main.WorldToScreenPoint(m_Transform.position);
         moveHorizontal = Input.GetAxis("Horizontal");
 		moveVertical = Input.GetAxis("Vertical");
+
+        //Boundary Check X-axis
+        if(handPosition.x > Screen.width && moveHorizontal > 0)
+        {
+            moveHorizontal = 0;
+        }
+        else if(handPosition.x < 0 && moveHorizontal < 0)
+        {
+            moveHorizontal = 0;
+        }
+        //Boundary Check Y-axis
+        if(handPosition.y > Screen.height && moveVertical > 0)
+        {
+            moveVertical = 0;
+        }
+        else if(handPosition.y < 0 && moveVertical < 0)
+        {
+            moveVertical = 0;
+        }
 		
-		m_Transform.position = transform.position + new Vector3(moveHorizontal * speed * Time.deltaTime, moveVertical * speed * Time.deltaTime, 0);
+		m_Transform.position = transform.position + new Vector3(moveHorizontal * cursorSpeed * Time.deltaTime, moveVertical * cursorSpeed * Time.deltaTime, 0);
     }
 }
