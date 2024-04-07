a3f9e786e0d90c07fbf476381a4d28696a6ce875
Cormac Dorrat <cormo905@gmail.com>
2024-04-07 20:39:41 +1000

    fix(unused code): removed unused code

--- MonstieWash/Assets/Scripts/Erasing/Tasks/ErasableTaskWrapper.cs
+++ MonstieWash/Assets/Scripts/Erasing/Tasks/ErasableTaskWrapper.cs
@@ -31,27 +31,4 @@ public class ErasableTaskWrapper : MonoBehaviour, ITask
         get { return m_newProgress; }
         set { NewProgress = value; }
     }
-
-    private void Awake()
-    {
-        //m_taskName = GetParentName(transform);
-    }
-
-    private string GetParentName(Transform parentTransform)
-    {
-        var taskName = "";
-
-        if (parentTransform.parent != null)
-        {
-            taskName += GetParentName(parentTransform.parent);
-        }
-        else
-        {
-            taskName += parentTransform.gameObject.scene.name + "Container";
-            return taskName;
-        }
-
-        taskName += $"#{parentTransform.name}";
-        return taskName;
-    }
 }
