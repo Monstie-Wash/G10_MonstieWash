657b063056839726ae732718c7703c0f57f30022
102101141 <102101141@student.swin.edu.au>
2024-04-13 13:53:33 +1000

    feat:(brush bubble particle system)
    Created bubble particle FX for use with the toothbrush tool. currently debugging why the tool isn't subscribing to the input manager properly

---
+++ MonstieWash/Assets/Scripts/ToolFX.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ToolFX : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particlesOnUse;
+    ParticleSystem myParticles;
+
+    private void Awake()
+    {
+        PlayerHand playerHand = FindFirstObjectByType<PlayerHand>();
+
+        myParticles = Instantiate(particlesOnUse, playerHand.transform);
+        //myParticles.Stop();
+    }
+
+        private void OnEnable()
+    {
+        InputManager.Inputs.OnActivate_Started += StartParticles;
+        InputManager.Inputs.OnActivate_Ended += StopParticles;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Inputs.OnActivate_Started -= StartParticles;
+        InputManager.Inputs.OnActivate_Ended -= StopParticles;
+    }
+
+        /// <summary>
+    /// Called every frame until the activate button is released.
+    /// </summary>
+    private void StartParticles()
+    {
+        Debug.Log("Subscribed");
+        myParticles.Play();
+    }
+
+    private void StopParticles() 
+    {
+        //myParticles.Stop();
+    }
+
+}
