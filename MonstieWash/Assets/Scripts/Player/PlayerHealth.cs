932a9c10f754decc30f58cdba0f6fb10c46dcd9f
102101141 <102101141@student.swin.edu.au>
2024-05-06 17:45:38 +1000

    Moving damage logic into new script playerHealth
    WIP

--- MonstieWash/Assets/Scripts/Player/PlayerHealth.cs
+++ MonstieWash/Assets/Scripts/Player/PlayerHealth.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
 
+    [Tooltip("Player's current health (initial value is starting health)")][SerializeField] private float playerHealth;
+    [Tooltip("Damage beyond this value won't affect the intensity of damage animations")][SerializeField] private float damageAnimationCap;
+    [Tooltip("Duration of the damage animation (in seconds)")][SerializeField] private float damageAnimationDuration;
+    [Tooltip("Animation curve for screen shake upon taking damage")][SerializeField] private AnimationCurve damageShake;
+    [Tooltip("The collider for being hit by attacks")][SerializeField] private Collider2D hitbox;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +20,31 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TakeDamage(3f);
+    }
+
+    public void TakeDamage(float dmgTaken)
+    {
+        playerHealth -= dmgTaken;
+        StartCoroutine(PlayDamageEffects(dmgTaken));
+    }
+
+    IEnumerator PlayDamageEffects(float dmgTaken)
+    {
+        var activeCam = Camera.main;
+        var camStartPos = activeCam.transform.position;
+        var elapsedTime = 0f;
+        var dmgNormal = Mathf.Clamp((dmgTaken / damageAnimationCap) + 1, 1, 2);
+
+        while (elapsedTime < damageAnimationDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            var strength = damageShake.Evaluate(elapsedTime / dmgTaken);
+            activeCam.transform.position = camStartPos + Random.insideUnitSphere * strength * dmgNormal;
+            yield return null;
+        }
+
+        activeCam.transform.position = camStartPos;
     }
 
 }
