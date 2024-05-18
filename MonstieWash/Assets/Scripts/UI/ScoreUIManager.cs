d24ae4fb9df89fff7670248b355f213c6aca7ce8
Acmetonto <141204538+100595153@users.noreply.github.com>
2024-05-18 15:07:24 +1000

    fix(scoreUI): Added changes as per PR comments.

--- MonstieWash/Assets/Scripts/UI/ScoreUIManager.cs
+++ MonstieWash/Assets/Scripts/UI/ScoreUIManager.cs
@@ -1,11 +1,8 @@
-using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public struct PHTask
@@ -14,8 +11,8 @@ public struct PHTask
     public bool complete;
     public int score;
 
-    public PHTask(string _name, bool _complete, int _score)
-    {  name = _name; complete = _complete; score = _score; }
+    public PHTask(string name, bool complete, int score)
+    {  this.name = name; this.complete = complete; this.score = score; }
 }
 
 public class ScoreUIManager : MonoBehaviour
@@ -23,7 +20,6 @@ public class ScoreUIManager : MonoBehaviour
     [SerializeField] private Image m_clipboard;
     [SerializeField] private GameObject m_lineItem;
     [SerializeField] private TextMeshProUGUI m_totalScore;
-    [SerializeField] private GameObject m_strikeOut;
 
     //Probably temporary, may change to calc scale with number of tasks later.
     [SerializeField][Range(0.0f, 5.0f)] private float m_spacing;
@@ -31,8 +27,8 @@ public class ScoreUIManager : MonoBehaviour
     /// <summary>
     /// Placeholder. Suggest Json to player prefs.
     /// </summary>
-    [SerializeField] private List<PHTask> m_tasks;
-    [SerializeField] private List<GameObject> m_LIList;
+    [SerializeField] private List<PHTask> m_tasks = new();
+    [SerializeField] private List<GameObject> m_LIList = new();
 
     [SerializeField][Range(0.0f, 2.0f)] private float m_secondsWait = 1.0f;
     private bool m_skip = false;
@@ -79,7 +75,7 @@ public class ScoreUIManager : MonoBehaviour
 
         int totalScore = 0;
 
-        for (int i = 0; i < m_tasks.Count; i++)
+        for (var i = 0; i < m_tasks.Count; i++)
         {
             m_LIList.Add(Instantiate(m_lineItem, clipTransform));
             m_LIList[i].SetActive(false);
@@ -95,9 +91,9 @@ public class ScoreUIManager : MonoBehaviour
             strikeOut.SetActive(false);
         }
 
-        for (int j = 0; j < m_LIList.Count; j++)
+        for (var i = 0; i < m_LIList.Count; i++)
         {
-            for (float timer = m_secondsWait; timer >= 0; timer -= Time.deltaTime)
+            for (float timer = m_secondsWait; timer >= 0f; timer -= Time.deltaTime)
             {
                 if (m_skip)
                 {
@@ -107,20 +103,18 @@ public class ScoreUIManager : MonoBehaviour
             }
 
             //Put the score in the score.
-            if (m_tasks[j].complete)
+            if (m_tasks[i].complete)
             {
-                score = m_LIList[j].transform.Find("TaskScore").GetComponent<TextMeshProUGUI>();
-                score.text = $"${m_tasks[j].score.ToString()}";
+                score = m_LIList[i].transform.Find("TaskScore").GetComponent<TextMeshProUGUI>();
+                score.text = $"${m_tasks[i].score.ToString()}";
 
-                strikeOut = m_LIList[j].transform.Find("StrikeOut").gameObject;
+                strikeOut = m_LIList[i].transform.Find("StrikeOut").gameObject;
                 strikeOut.SetActive(true);
 
                 //Add to score total
-                totalScore += m_tasks[j].score;
+                totalScore += m_tasks[i].score;
             }
         }
-
-
 
         m_totalScore.text = $"${totalScore.ToString()}";
     }
