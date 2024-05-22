a8cd4f0a694dd3f0799bb17763940dc852fee2ee
LucaPalma <103647073@student.swin.edu.au>
2024-05-22 22:16:15 +1000

    Fix: Eye Track Sprites Correctly

--- MonstieWash/Assets/Scripts/MonsterAi/EyeFollowObj.cs
+++ MonstieWash/Assets/Scripts/MonsterAi/EyeFollowObj.cs
@@ -1,72 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class EyeFollowObj : MonoBehaviour
 {
     private PlayerHand m_ph;
-    private Vector3 pupilStartingPos;
 
-    public float eyeSpeed;
-    public float trackingSpeed;
-    public float bufferFromEdge;
-    public GameObject pupil;
-    public LayerMask eyeLayer;
-    public SpriteRenderer spriteRenderer;
-    public PolygonCollider2D polyCollider;
+    [SerializeField] private float eyeSpeed;
+    [SerializeField] private float trackingSpeed;
+    [SerializeField] private float bufferFromEdge;
+    [SerializeField] private List<Pupil> pupils;
+    [SerializeField] private LayerMask eyeLayer;
+    private SpriteRenderer m_spriteRenderer;
+    private PolygonCollider2D m_polyCollider;
 
     private Sprite m_spriteLastFrame;
+
+    [Serializable]
+    public class Pupil
+    {
+        public GameObject pupilObj;
+        public Vector2 pupilStartingPos;
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_polyCollider = GetComponent<PolygonCollider2D>();
         m_ph = FindFirstObjectByType<PlayerHand>();
-        pupilStartingPos = pupil.transform.position;
+
+        foreach( Pupil p in pupils)
+        {
+            p.pupilStartingPos = p.pupilObj.transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Have to check pupil exists within animation as some eyes don't have one.
-        pupil = this.transform.GetChild(0).gameObject;
-        if (pupil == null) return;
-
         //Check if sprite has changed due to animation and if so regenerate collider shape.
-        if (m_spriteLastFrame != spriteRenderer.sprite)
+        if (m_spriteLastFrame != m_spriteRenderer.sprite)
         {
-            if (polyCollider != null && spriteRenderer != null)
+            if (m_polyCollider != null && m_spriteRenderer != null)
             {
                 //Get new physics shape count.
-                polyCollider.pathCount = spriteRenderer.sprite.GetPhysicsShapeCount();
+                m_polyCollider.pathCount = m_spriteRenderer.sprite.GetPhysicsShapeCount();
 
                 //Recreate the path component of collider.
                 List<Vector2> path = new List<Vector2>();
 
                 //Loop through path and generate based on physics shape.
-                for (int i = 0; i < polyCollider.pathCount; i++)
+                for (int i = 0; i < m_polyCollider.pathCount; i++)
                 {
                     path.Clear();
-                    spriteRenderer.sprite.GetPhysicsShape(i, path);
-                    polyCollider.SetPath(i, path.ToArray());
+                    m_spriteRenderer.sprite.GetPhysicsShape(i, path);
+                    m_polyCollider.SetPath(i, path.ToArray());
                 }
             }
         }
 
-        //Cast ray from hand towards eye to find the outer edge of the eye.
-        var cast = Physics2D.Raycast(m_ph.transform.position, pupilStartingPos - m_ph.transform.position, Mathf.Infinity, eyeLayer);
-        if (!cast) { print("Cast couldn't find Eye obj"); return; }
+        foreach (Pupil p in pupils)
+        {
+            //Change playerhand transform to vector 2.
+            var playerHandVec = new Vector2(m_ph.transform.position.x, m_ph.transform.position.y);
 
-        
-        //Convert pupil world space and player hand to 2D directional vector.
-        var dir = new Vector2(pupilStartingPos.x, pupilStartingPos.y) - new Vector2(m_ph.transform.position.x, m_ph.transform.position.y);
-        //Calculate point back from closest point offset by the buffer.
-        var newPupilPoint = Vector2.Lerp(pupil.transform.position, cast.point + dir.normalized * bufferFromEdge, eyeSpeed * Time.deltaTime);
+            //Cast ray from hand towards eye to find the outer edge of the closest eye.
+            var cast = Physics2D.RaycastAll(m_ph.transform.position, p.pupilStartingPos - playerHandVec, Mathf.Infinity, eyeLayer);
+            if (cast.Length == 0) { print("Cast couldn't find Eye obj"); return; }
 
-        //Move the pupil towards the calculated point by the follow speed.
-        pupil.transform.position = Vector2.Lerp(pupil.transform.position, newPupilPoint, eyeSpeed * Time.deltaTime);
+            //Find out which cast collision is closest to its starting point.
+            var bestCast = new Vector2(-999999, -999999);
+
+            foreach(RaycastHit2D hit in cast)
+            {
+                //Get distance from hit to pupil starting pos
+                var hitDist = Vector2.Distance(hit.point, p.pupilStartingPos);
+
+                //Update bestCast position if distance is less
+                if (hitDist < Vector2.Distance(bestCast, p.pupilStartingPos)) bestCast = hit.point;
+            }
 
 
-        m_spriteLastFrame = spriteRenderer.sprite;
+            //Convert pupil world space and player hand to 2D directional vector.
+            var dir = new Vector2(p.pupilStartingPos.x, p.pupilStartingPos.y) - new Vector2(m_ph.transform.position.x, m_ph.transform.position.y);
+
+            //Calculate point back from closest point offset by the buffer.
+            var newPupilPoint = Vector2.Lerp(p.pupilObj.transform.position, bestCast + dir.normalized * bufferFromEdge, eyeSpeed * Time.deltaTime);
+
+            //Move the pupil towards the calculated point by the follow speed.
+            p.pupilObj.transform.position = Vector2.Lerp(p.pupilObj.transform.position, newPupilPoint, eyeSpeed * Time.deltaTime);
+        }
+
+        m_spriteLastFrame = m_spriteRenderer.sprite;
         
     }
 }
