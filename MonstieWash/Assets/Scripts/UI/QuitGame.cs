c90b8362ac552e527d0cc92223a0d91f98363926
Nathan Trau <103618066@student.swin.edu.au>
2024-05-24 16:08:41 +1000

    fix(death scene): fixed quit button

--- MonstieWash/Assets/Scripts/UI/QuitGame.cs
+++ MonstieWash/Assets/Scripts/UI/QuitGame.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void Quit()
     { 
-        Application.Quit();
         Debug.Log("Quitting...");
+        Application.Quit();
     }
 }
