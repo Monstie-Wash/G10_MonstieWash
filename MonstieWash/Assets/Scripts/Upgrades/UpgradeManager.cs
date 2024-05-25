9f521ba5ef93972c7be1ed0d70b1ef776d617714
Nathan Trau <103618066@student.swin.edu.au>
2024-05-25 21:37:54 +1000

    feat(upgrade scene): Added upgrade scene for purchasing upgrades

---
+++ MonstieWash/Assets/Scripts/Upgrades/UpgradeManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpgradeManager : MonoBehaviour
+{
+    [SerializeField] private GameObject toolUpgradePrefab;
+    [SerializeField] private Transform upgradeContainer;
+    [SerializeField] private Tool[] upgradableTools;
+    [SerializeField] private TextMeshProUGUI scoreUI;
+
+    private int m_currentScore = 150;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        foreach (var tool in upgradableTools)
+        {
+            var upgradeObject = Instantiate(toolUpgradePrefab, upgradeContainer);
+            var upgradeScript = upgradeObject.GetComponent<ToolUpgrade>();
+
+            Debug.Log(upgradeObject.name);
+            Debug.Log(tool.name);
+
+            upgradeScript.SetTool(tool);
+        }
+    }
+
+    private void OnEnable()
+    {
+        m_currentScore += 150;
+        UpdateScoreUI();
+    }
+
+    private void UpdateScoreUI()
+    {
+        if (scoreUI != null) 
+        {
+            scoreUI.text = $"${m_currentScore.ToString()}";
+        }
+    }
+
+    public bool SpendScore(int score)
+    {
+        var result = false;
+        if (score <= m_currentScore)
+        {
+            m_currentScore -= score;
+            UpdateScoreUI();
+            result = true;
+        }
+        return result;
+    }
+}
