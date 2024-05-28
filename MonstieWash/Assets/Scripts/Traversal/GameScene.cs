fabdae9dad69830c9f257b222e3d3b624cd71f0c
Cormac <cormo905@gmail.com>
2024-05-28 13:49:11 +1000

    Cleared Main

--- MonstieWash/Assets/Scripts/Traversal/GameScene.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameScene", menuName = "ScriptableObjects/GameScene")]
-public class GameScene : ScriptableObject
-{
-    [SerializeField] private string sceneName;
-    [HideInInspector] public string SceneName { get { return sceneName; } }
-}
