720e4fdfd88a5efe6f0cfa96c3a132a7ff4f7c74
Cormac <cormo905@gmail.com>
2024-06-04 17:28:42 +1000

    feat(erasing): erasing now works for transformed sprites
    Still has strange behaviour when erasables are scaled down for some reason.

--- MonstieWash/Assets/Scripts/Erasing/Tool.cs
+++ MonstieWash/Assets/Scripts/Erasing/Tool.cs
@@ -37,7 +37,7 @@ public class Tool : ScriptableObject
         //The input strength is what the user will adjust to control the strength of the tool.
         //The input strength is scaled exponentially to provide a smoother strength gradient and improved user experience.
         var exponentBase = 1.0472f; //This is a value that comes from the 100th root of 100. This is to ensure 100% input strength matches 100% output strength.
-        Strength = Mathf.Pow(exponentBase, inputStrength);
+        Strength = Mathf.Clamp(Mathf.Pow(exponentBase, inputStrength), 1f, 100f);
 
         MaskPixels = new byte[mask.texture.width * mask.texture.height];
         Color[] maskColors = mask.texture.GetPixels();
