7cc7eee47d190ac5239f71d18f73079e3a3ce659
Nathan Trau <103618066@student.swin.edu.au>
2024-07-16 19:30:51 +1000

    feat(compendium manager): added compendium manager to show/hide different compendium entry pages when hovered over

---
+++ MonstieWash/Assets/Scripts/Compendium/CompendiumEntry.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CompendiumEntry : MonoBehaviour, IPointerEnterHandler
+{
+    public string Name { get; private set; }
+    public string Temperament { get; private set; }
+    public string Description { get; private set; }
+    public Image ImageOriginal { get; private set; }
+    public Image ImageCleared { get; private set; }
+    public bool Completed { get; set; }
+
+    [SerializeField] private string name;
+    [SerializeField] private string temperament;
+    [SerializeField] private string description;
+    [SerializeField] private Image imageOriginal;
+    [SerializeField] private Image imageCleared;
+
+    private CompendiumManager m_compendiumManager;
+
+    private void Start()
+    {
+        Name = name;
+        Temperament = temperament;
+        Description = description;
+        ImageOriginal = ImageOriginal;
+        ImageCleared = ImageCleared;
+        Completed = false;
+
+        m_compendiumManager = FindFirstObjectByType<CompendiumManager>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_compendiumManager.DisplayEntry(this);
+    }
+}
---
+++ MonstieWash/Assets/Scripts/Compendium/CompendiumManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CompendiumManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textTitle;
+    [SerializeField] private Image imageOriginal;
+    [SerializeField] private Image imageCleared;
+    [SerializeField] private TextMeshProUGUI textTemperament;
+    [SerializeField] private TextMeshProUGUI textDescription;
+
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    public void DisplayEntry(CompendiumEntry entry)
+    {
+        if (entry == null) return;
+
+        if(!transform.GetChild(0).gameObject.activeSelf)
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+
+        textTitle.text = entry.Name;
+        imageOriginal = entry.ImageOriginal;
+        textTemperament.text = entry.Temperament;
+        textDescription.text = entry.Description;
+
+        if (entry.Completed) imageCleared = entry.ImageCleared;
+    }
+}
