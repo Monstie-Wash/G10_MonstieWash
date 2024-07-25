b4e833c5d6ceb2d464d211a55affd96d70900b88
Cormac <cormo905@gmail.com>
2024-07-25 22:07:09 +1000

    feat(bedroom): added all bedroom scenes and connections
    Bedroom scene is now the scene that opens on default and is where the player is sent back to after a level.

--- MonstieWash/Assets/Scripts/Traversal/GameSceneManager.cs
+++ MonstieWash/Assets/Scripts/Traversal/GameSceneManager.cs
@@ -9,6 +9,8 @@ public class GameSceneManager : MonoBehaviour
     public event Action OnScenesLoaded;
     public event Action<string> OnSceneChanged;
 
+    [SerializeField] private GameScene initialScene;
+    [SerializeField] private List<GameScene> bedroomScenes;
     [SerializeField] private GameScene levelSelectScene;
     [SerializeField] private GameScene loadingScene;
     [SerializeField] private GameScene scoreSummaryScene;
@@ -60,15 +62,23 @@ public class GameSceneManager : MonoBehaviour
     private async void LoadMenuScenes()
     {
         await LoadScene(loadingScene.SceneName);
-        await LoadScene(levelSelectScene.SceneName);
-        await LoadScene(upgradeScene.SceneName);
 
+        await LoadScene(initialScene.SceneName);
+        foreach (GameScene scene in bedroomScenes)
+        {
+            await LoadScene(scene.SceneName);
+            SetSceneActive(scene.SceneName, false);
+        }
+        await LoadScene(levelSelectScene.SceneName);
+        SetSceneActive(levelSelectScene.SceneName, false);
+        await LoadScene(upgradeScene.SceneName);
         SetSceneActive(upgradeScene.SceneName, false);
+
         SetSceneActive(loadingScene.SceneName, false);
 
-        InputManager.Instance.SetCursorMode(false);
-        InputManager.Instance.SetControlScheme(InputManager.ControlScheme.MenuActions);
-        m_currentScene = SceneManager.GetSceneByName(levelSelectScene.SceneName);
+        InputManager.Instance.SetCursorMode(true);
+        InputManager.Instance.SetControlScheme(InputManager.ControlScheme.PlayerActions);
+        m_currentScene = SceneManager.GetSceneByName(initialScene.SceneName);
     }
 
     /// <summary>
@@ -181,6 +191,29 @@ public class GameSceneManager : MonoBehaviour
     }
 
     /// <summary>
+    /// Swaps the current active scene to a new scene
+    /// </summary>
+    /// <param name="target">The scene to move to.</param>
+    public void MoveToScene(string target, bool targetIsUI)
+    {
+        SetSceneActive(m_currentScene.name, false);
+        SetSceneActive(target, true);
+
+        if (targetIsUI)
+        {
+            InputManager.Instance.SetCursorMode(false);
+            InputManager.Instance.SetControlScheme(InputManager.ControlScheme.MenuActions);
+        }
+        else
+        {
+            InputManager.Instance.SetCursorMode(true);
+            InputManager.Instance.SetControlScheme(InputManager.ControlScheme.PlayerActions);
+        }
+
+        OnSceneChanged?.Invoke(target);
+    }
+
+    /// <summary>
     /// Starts a level from the beginning. Intended to be run from the level select scene.
     /// </summary>
     /// <param name="level"></param>
@@ -233,7 +266,7 @@ public class GameSceneManager : MonoBehaviour
         MoveToScene(deathScene.SceneName);
     }
 
-    public async void GoToUpgradeMenu()
+    public void GoToUpgradeMenu()
     {
         MoveToScene(loadingScene.SceneName);
 
@@ -255,9 +288,9 @@ public class GameSceneManager : MonoBehaviour
         await UnloadActiveLevelScenes();
 
         GetComponentInChildren<MusicManager>().SetMusic(MusicManager.MusicType.Morning);
-        InputManager.Instance.SetCursorMode(false);
-        InputManager.Instance.SetControlScheme(InputManager.ControlScheme.MenuActions);
-        MoveToScene(levelSelectScene.SceneName);
+        InputManager.Instance.SetCursorMode(true);
+        InputManager.Instance.SetControlScheme(InputManager.ControlScheme.PlayerActions);
+        MoveToScene(initialScene.SceneName);
     }
 
     /// <summary>
