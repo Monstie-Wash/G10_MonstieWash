a7b6a5ae638e5c08d62616f064cdf096bbc0eca0
Acmetonto <141204538+100595153@users.noreply.github.com>
2024-10-04 21:02:43 +1000

    fix(removed clipboard): it's gone! where'd it go?

--- MonstieWash/Assets/Scripts/UI/UIManager.cs
+++ MonstieWash/Assets/Scripts/UI/UIManager.cs
@@ -7,13 +7,8 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private Image clipboard;
-    [SerializeField] private bool autoHideClipboard = true;
-    [SerializeField] private float clipboardAutoHideDelay = 5f;
-    [SerializeField] private Animator CBAnimator;
     [SerializeField] private GameObject taskContainer;
     [SerializeField] private GameObject taskTextPrefab;
-    [SerializeField] private TextMeshProUGUI completionText;
     [SerializeField] private float fontSize = 36f;
     [SerializeField] private float fontBuffer = 1.25f;
 	[Space(20), SerializeField] private GameObject finishLevelButton;
@@ -25,51 +20,34 @@ public class UIManager : MonoBehaviour
     private Dictionary<TaskType, bool> m_taskList = new();
 
     private TaskTracker m_taskTracker;
-    private ProgressBarUI[] m_progressBars; // Array is overhead if we have multiple progress bars to track scene vs total completion
-    private Coroutine m_clipboardAutoHide;
 
     public static event Action OnTaskHide;
 
     private void Awake()
     {
         m_taskTracker = FindFirstObjectByType<TaskTracker>();
-        m_progressBars = FindObjectsByType<ProgressBarUI>(FindObjectsSortMode.None);
 
         Time.timeScale = 0f;
     }
 
     private void OnEnable()
     {
-        InputManager.Instance.OnToggleUI += Inputs_OnToggleUI;
+        //InputManager.Instance.OnToggleUI += Inputs_OnToggleUI;
         m_taskTracker.OnLevelCompleted += OnLevelCompleted;
         m_taskTracker.OnSceneCompleted += OnSceneCompleted;
     }
 
     private void OnDisable()
     {
-        InputManager.Instance.OnToggleUI -= Inputs_OnToggleUI;
+        //InputManager.Instance.OnToggleUI -= Inputs_OnToggleUI;
         m_taskTracker.OnLevelCompleted -= OnLevelCompleted;
         m_taskTracker.OnSceneCompleted -= OnSceneCompleted;
     }
 
-    private void Inputs_OnToggleUI()
-    {
-        ToggleUIVisibility(CBAnimator);
-    }
     private void ToggleUIVisibility(Animator animator)
     {
         var clipboardHidden = animator.GetBool("Hide");
         animator.SetBool("Hide", !clipboardHidden);
-
-        if (!autoHideClipboard) return;
-        if (clipboardHidden) m_clipboardAutoHide = StartCoroutine(ClipboardHideTimer());
-        else StopCoroutine(m_clipboardAutoHide);
-    }
-
-    private IEnumerator ClipboardHideTimer()
-    {
-        yield return new WaitForSeconds(clipboardAutoHideDelay);
-        ToggleUIVisibility(CBAnimator);
     }
 
 /// <summary>
@@ -86,26 +64,7 @@ public class UIManager : MonoBehaviour
             }
         }
 
-        InitialiseClipboard();
         InitialiseStartList();
-    }
-
-/// <summary>
-/// Recursively creates objects inside the clipboard object for each layer of tasks across all monster scenes. 
-/// </summary>
-/// <param name=""></param>
-    private void InitialiseClipboard()
-    {
-        foreach (var pair in m_taskList)
-        {
-            var newTaskObject = Instantiate(taskTextPrefab, taskContainer.transform);
-            
-            newTaskObject.name = pair.Key.ToString();
-
-            var newTaskText = newTaskObject.GetComponent<TextMeshProUGUI>();
-            newTaskText.fontSize = fontSize;
-            newTaskText.text = Resources.Load<TaskDesc>(newTaskObject.name).description;
-        } 
     }
 
     private void InitialiseStartList()
@@ -121,36 +80,6 @@ public class UIManager : MonoBehaviour
 			newTaskText.text = Resources.Load<TaskDesc>(newTaskObject.name).description;
 		}
 	}
-
-/// <summary>
-/// Updates the progress value displayed on the clipboard for a given task.
-/// </summary>
-/// <param name="scene">String identifier of the task to update.</param>
-    public void UpdateClipboardTask(TaskType type)
-    {
-		TextMeshProUGUI taskText = taskContainer.transform.Find(type.ToString()).GetComponent<TextMeshProUGUI>();
-		taskText.text = $"<s>{taskText.text}</s>";
-    }
-
-	/// <summary>
-	/// Updates the progress bar based on completion percentage.
-	/// </summary>
-    public void UpdateProgressBar(float overallCompletion)
-    {
-        foreach(var progressBar in m_progressBars)
-        {
-            progressBar.UpdateUI(overallCompletion);
-        }
-	}
-	
-    /// <summary>
-    /// Updates the completion percentage on the clipboard for the current scene.
-    /// </summary>
-    /// <param name="overallCompletion">The overall completion to display.</param>
-    public void UpdateCompletion(float overallCompletion)
-    {
-        completionText.text = $"{Mathf.CeilToInt(overallCompletion * 100f)}%";
-    }
 
     private void OnLevelCompleted()
     {
@@ -177,7 +106,6 @@ public class UIManager : MonoBehaviour
     public void OnTaskListHide()
     {
 		ToggleUIVisibility(taskListAnim);
-		ToggleUIVisibility(CBAnimator);
         Time.timeScale = 1.0f;
         OnTaskHide?.Invoke();
     }
