ed3684313b052580b285201f3dc5ac50ee64095a
Cormac <cormo905@gmail.com>
2024-10-26 18:05:31 +1100

    Cleared Main

--- MonstieWash/Assets/Scripts/Tasks/TaskDesc.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/TaskDesc", order = 1)]
-public class TaskDesc : ScriptableObject
-{
-    public string description;
-}
